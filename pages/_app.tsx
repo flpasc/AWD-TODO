8f8b96fa0545607bc3735f4e30e3250bf49609c1
Florian <florianpaulschoene@gmail.com>
2025-07-04 11:37:12 +0200

    init

---
+++ pages/_app.tsx
@@ -0,0 +1,34 @@
+import '@/styles/globals.css'
+import { ChakraProvider, theme, ColorModeScript } from '@chakra-ui/react'
+import { fonts } from '../lib/fonts'
+import { SWRConfig } from 'swr'
+import { AppProps } from 'next/app'
+
+const fetcher = (url: string): Promise<> =>
+	fetch(url).then((response) => response.json())
+
+export default function App({ Component, pageProps }: AppProps) {
+	return (
+		<>
+			<style
+				jsx
+				global
+			>
+				{`
+					:root {
+						--font-rubik: ${fonts.rubik.style.fontFamily};
+					}
+					body,
+					html {
+						height: 100%;
+					}
+				`}
+			</style>
+			<ChakraProvider theme={theme}>
+				<SWRConfig value={{ fetcher }}>
+					<Component {...pageProps} />
+				</SWRConfig>
+			</ChakraProvider>
+		</>
+	)
+}
